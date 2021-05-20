2e95a2188056922c33da41c47fe5ae5724c8cdc3
ihfazhillah <mihfazhillah@gmail.com>
2021-05-20 13:30:17 +0700

    change package name

--- app/src/main/java/io/jupita/demo/MainActivity.kt
+++ app/src/main/java/io/jupita/demo/MainActivity.kt
@@ -2,7 +2,6 @@ package io.jupita.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ihfazh.jupitaagentexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
