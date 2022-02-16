92a0d6907ef32a06093a12db71051954fb4cb001
Jupita.io <78889064+Jupita-io@users.noreply.github.com>
2022-02-16 23:03:59 +1100

    Update IRequest.kt

--- jupita/src/main/java/io/jupita_kt/network/IRequest.kt
+++ jupita/src/main/java/io/jupita_kt/network/IRequest.kt
@@ -7,6 +7,7 @@ interface IRequest {
     fun dump(
         text: String,
         input_id: String,
+        channel_type: String,
         type: Int,
         isCall: Boolean,
         dumpListener: DumpListener?
